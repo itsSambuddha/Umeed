f5e77e7b845af1933d8f7d066f19d3659e07d991
itsSambuddha <139554092+itsSambuddha@users.noreply.github.com>
2025-05-17 19:23:16 +0530

    commit_login_test

---
+++ file.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
+
+completion = client.chat.completions.create(
+  extra_headers={
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+  },
+  model="google/gemini-2.0-flash-exp:free",
+  messages=[
+    {
+      "role": "user",
+      "content": "What is the meaning of life?"
+    }
+  ]
+)
+
+print(completion.choices[0].message.content)
